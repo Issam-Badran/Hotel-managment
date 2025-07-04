61a23f4014f32e1c9e23dabe5ad806c0b82b28cc
Issam-Badran <162922278+Issam-Badran@users.noreply.github.com>
2025-07-04 14:01:10 +0300

    Update api.php

--- api.php
+++ api.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('guests', GuestController::class);
 
 
+// Could be instead:
+
+// Route::post('/guests', [GuestController::class, 'store']);
+
+// Route::get('/guests', [GuestController::class, 'index']);
+
+// Route::get('/guests/{id}', [GuestController::class, 'show']);
+
+// Route::patch('/guests/{id}', [GuestController::class, 'update']);
+
+// Route::delete('/guests/{id}', [GuestController::class, 'destroy']);
