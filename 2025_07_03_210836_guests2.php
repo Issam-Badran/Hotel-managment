0e367f02da28953a75b44fc52a958b37ccad8322
Issam-Badran <162922278+Issam-Badran@users.noreply.github.com>
2025-07-04 13:46:51 +0300

    Update 2025_07_03_210836_guests2.php

--- 2025_07_03_210836_guests2.php
+++ 2025_07_03_210836_guests2.php
@@ -14,7 +14,7 @@ return new class extends Migration
     Schema::create('guests', function (Blueprint $table) {
         $table->id();
         $table->string('name');
-        $table->string('phone');
+        $table->string('phone',20);
         $table->string('email')->unique();
         $table->timestamps();
     });
